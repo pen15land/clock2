038bf58d52cedb63eed39f69ddbd7dbdcba19820
caslabs <haldaco2003@gmail.com>
2020-01-17 10:05:28 -1000

    fourth commit

--- app.js
+++ app.js
@@ -3,23 +3,10 @@ function Time(){
   var hour = date.getHours();
   var minutes = date.getMinutes();
   var seconds = date.getSeconds();
-  hour = updateTime(hour);
-  minutes = updateTime(minutes);
-  seconds = updateTime(seconds);
   document.getElementById("Time").innerText = hour + " : " + minutes + " : " + seconds;
-  var t = setTimeout(function(){ Time() }, 1000);
 }
-function updateTime(k) {
-  if (k < 10) {
-    return "0" + k;
-  }
-  else {
-    return k;
-  }
-}
-
 Time();
 
 function Bk(color){
         document.body.style.background = color;
-};
+};s
